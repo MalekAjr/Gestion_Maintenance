43f783ca600ab311b460161cf4a68c5cf851aee4
MalekAjr <malekajroud18@gmail.com>
2024-09-17 19:36:38 +0100

    Fix issues in UpdateCar.jsx component

--- front/src/components/admin/UpdateCar.jsx
+++ front/src/components/admin/UpdateCar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import adminService from '../../services/adminService';
-import cors from 'cors';
 import { BsFillArrowLeftSquareFill } from 'react-icons/bs';
 
 const UpdateCar = () => {
