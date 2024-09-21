dd6023e3bd4185b403ee61f6975e92df93f2f9fb
MalekAjr <malekajroud18@gmail.com>
2024-09-21 01:45:44 +0100

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,15 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
+app.use(cors());
 const mongoose = require('mongoose');
 const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static('public'));
-app.use('/usersImages', express.static(path.join(__dirname, 'public', 'usersImages')));
-app.use('/piecesImages', express.static(path.join(__dirname, 'public', 'piecesImages')));
-app.use('/fichesImages', express.static(path.join(__dirname, 'public', 'fichesImages')));
-
 const server = http.createServer(app);
+
+
 const io = socketIo(server, {
     cors: {
         origin: '*',
@@ -21,28 +17,36 @@ const io = socketIo(server, {
     }
 });
 
+app.use(express.static('public'));
+app.use('/usersImages', express.static(path.join(__dirname, 'public', 'usersImages')));
+app.use('/piecesImages', express.static(path.join(__dirname, 'public', 'piecesImages')));
+app.use('/fichesImages', express.static(path.join(__dirname, 'public', 'fichesImages')));
+
+app.use(cors({ origin: '*' }));
+
+
+
 mongoose.connect("mongodb+srv://baabadevs:admin123@mernapp.gendjkv.mongodb.net/Gestion_Maintenance")
     .then(() => {
         console.log("Connected to mongoose");
 
         // Require routes after successful connection
         const fiche_route = require('./routes/ficheinterventionRoute');
+        app.use('/api', fiche_route);
         const userrouter = require('./routes/userRoute');
+        app.use('/api', userrouter);
         const eventrouter = require('./routes/eventRoute');
+        app.use('/api', eventrouter);
         const carrouter = require('./routes/carRoute');
+        app.use('/api', carrouter);
         const ordremissionrouter = require('./routes/ordremissionRoute');
+        app.use('/api', ordremissionrouter);
         const ticketrouter = require('./routes/ticketRoute');
+        app.use('/api', ticketrouter);
         const mailrouter = require('./routes/mailRoute');
+        app.use('/api', mailrouter);
         const notificationrouter = require('./routes/notificationRoute');
-
-        app.use('/api/fiche', fiche_route);
-        app.use('/api/user', userrouter);
-        app.use('/api/event', eventrouter);
-        app.use('/api/car', carrouter);
-        app.use('/api/ordremission', ordremissionrouter);
-        app.use('/api/ticket', ticketrouter);
-        app.use('/api/mail', mailrouter);
-        app.use('/api/notification', notificationrouter);
+        app.use('/api', notificationrouter);
 
         app.set('io', io);
     })
@@ -50,20 +54,17 @@ mongoose.connect("mongodb+srv://baabadevs:admin123@mernapp.gendjkv.mongodb.net/G
         console.error("Error connecting to mongoose:", error);
     });
 
-io.on('connection', (socket) => {
-    console.log('New client connected');
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
+    io.on('connection', (socket) => {
+        console.log('New client connected');
+        socket.on('disconnect', () => {
+            console.log('Client disconnected');
+        });
     });
-});
 
-// Middleware pour gérer les requêtes 404
-app.use((req, res) => {
-    res.status(404).send('404: Not Found');
-});
+const PORT = 8000;
 
-const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-});
+  });
+  
