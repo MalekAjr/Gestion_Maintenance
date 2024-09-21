afb4b2358fcdba371e64389d7b8e108b42e27e84
MalekAjr <malekajroud18@gmail.com>
2024-09-21 03:33:14 +0100

    Add backend deployment to vercel.json

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors({ origin: '*' }));
 const mongoose = require('mongoose');
 const path = require('path');
-const http = require('http');
 const socketIo = require('socket.io');
 
-const server = http.createServer(app);
-
-
-const io = socketIo(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-});
-
+app.use(cors());
 app.use(express.static('public'));
 app.use('/usersImages', express.static(path.join(__dirname, 'public', 'usersImages')));
 app.use('/piecesImages', express.static(path.join(__dirname, 'public', 'piecesImages')));
 app.use('/fichesImages', express.static(path.join(__dirname, 'public', 'fichesImages')));
-
-
-
 
 mongoose.connect("mongodb+srv://baabadevs:admin123@mernapp.gendjkv.mongodb.net/Gestion_Maintenance")
     .then(() => {
@@ -46,25 +32,25 @@ mongoose.connect("mongodb+srv://baabadevs:admin123@mernapp.gendjkv.mongodb.net/G
         app.use('/api', mailrouter);
         const notificationrouter = require('./routes/notificationRoute');
         app.use('/api', notificationrouter);
-
-        app.set('io', io);
     })
     .catch(error => {
         console.error("Error connecting to mongoose:", error);
     });
 
-    io.on('connection', (socket) => {
-        console.log('New client connected');
-        socket.on('disconnect', () => {
-            console.log('Client disconnected');
-        });
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+});
+
+const io = socketIo(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
     });
-
-const PORT = 8000;
-
-
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-  
-  module.exports = app;
+});
