710163d5550f4962abe0ee00640b5a3c994534c5
MalekAjr <malekajroud18@gmail.com>
2024-09-24 00:46:30 +0100

    after changing  vercel.json in backendn and the domain

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,14 @@ const socketIo = require('socket.io');
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }));app.use(express.static('public'));
+  }));
+  
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+  app.use(express.static('public'));
 app.use('/usersImages', express.static(path.join(__dirname, 'public', 'usersImages')));
 app.use('/piecesImages', express.static(path.join(__dirname, 'public', 'piecesImages')));
 app.use('/fichesImages', express.static(path.join(__dirname, 'public', 'fichesImages')));
